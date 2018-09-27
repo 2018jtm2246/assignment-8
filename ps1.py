26a1e2ba725873572379ecdd589eb6228689a81d
2018jtm2246 <rahul.girotra2011@gmail.com>
2018-09-27 14:32:55 +0530

    this is my first updated file of program 1

--- ps1.py
+++ ps1.py
@@ -1,3 +1,14 @@
-###### this is the first .py file ###########
+n=int(input("enter the value of rows n :"))
+m=int(input("enter the value of coloumn m:"))
+matrix  = []
 
-####### write your code here ##########
+for i in range(0,m):
+	matrix.append([])
+for i in range(0,n):
+	for j in range(0,m):
+		matrix[i].append(j)
+		matrix[i][j]=0
+for i in range(0,n):
+	for j in range(0,m):
+	 matrix[i][j] = input()
+print(matrix)
