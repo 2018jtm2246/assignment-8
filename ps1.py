ed78642c184a12e097700bc19cf118ac3f07c7ab
2018jtm2246 <rahul.girotra2011@gmail.com>
2018-09-27 19:29:18 +0530

    this is my last updated file of program 1

--- ps1.py
+++ ps1.py
@@ -1,8 +1,8 @@
-n=int(input("enter the value of rows n :"))
+n=int(input("enter the value of rows n :"))  #entering the number of rows and coloumns
 m=int(input("enter the value of coloumn m:"))
 matrix  = []
 
-for i in range(0,m):
+for i in range(0,m):       #entering the various elements of matrix from the user
 	matrix.append([])
 for i in range(0,n):
 	for j in range(0,m):
@@ -15,14 +15,15 @@ print(matrix)
 
 
 
-for i in range(0,m):
+for i in range(0,m):  #checking the different patterns in the matrix and accordingly printing the data
 	for j in range(0,n):
-	`	if matrix[i][j]=="S" and matrix[i-1][j]=="S" and matrix[i-2][j]=="S" and matrix[i+1][j]=="S" and matrix[i+2][j]=="S" and matrix[i][j+1]=="S" and matrix[i][j+2]=="S" and matrix[i][j-1]=="S" and matrix[i][j-2]=="S" :
-			print(9,5)
+		if matrix[i][j]=="S" :
+			if  matrix[i-1][j]=="S" and matrix[i-2][j]=="S" and matrix[i+1][j]=="S" and matrix[i+2][j]=="S" and matrix[i][j+1]=="S" and matrix[i][j+2]=="S" and matrix[i][j-1]=="S" and matrix[i][j-2]=="S":
+				print("9,5")
 		
-		elif matrix[i][j]=="S" and matrix[i-1][j]=="S" and matrix[i+1][j]=="S" and matrix[i][j-1]=="S" and matrix[i][j+1]=="S" :
-			print(5,1)
+			elif  matrix[i-1][j]=="S" and matrix[i+1][j]=="S" and matrix[i][j-1]=="S" and matrix[i][j+1]=="S" :
+				print("5,1")
 		
 		else:
-			 print(1)
+			print("1")
 
