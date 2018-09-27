d8f57b53a41aefd4635efc255f564eea0648cc5d
2018jtm2246 <rahul.girotra2011@gmail.com>
2018-09-27 14:39:00 +0530

    this is my first updated file of program 2

--- ps2.py
+++ ps2.py
@@ -1,3 +1,26 @@
 ###### this is the second .py file ###########
 
 ####### write your code here ##########
+group1 = list("abcdefghi")
+group2 = list("jklmnopqr")
+group3 = list("stuvwxyz_")
+
+gr1=[]
+gr2=[]
+gr3=[]
+gr1new=[]
+gr2new=[]
+gr3new=[]
+a1,a2,a3 = [int(i) for i in input().split()]
+
+word=input()
+list(word)
+
+	for i in word[:]:
+    		if i in group1:
+        		gr1.append(i)
+    		elif i in group2:
+        		gr2.append(i)
+    		elif i in group3:
+        		gr3.append(i)
+
