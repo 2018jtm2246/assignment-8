afcba78c51cd37774b2994ef979caa5bd3f150b4
2018jtm2246 <rahul.girotra2011@gmail.com>
2018-09-27 16:18:22 +0530

    this is my second updated file of program 1

--- ps1.py
+++ ps1.py
@@ -12,3 +12,17 @@ for i in range(0,n):
 	for j in range(0,m):
 	 matrix[i][j] = input()
 print(matrix)
+
+
+
+for i in range(0,m):
+	for j in range(0,n):
+	`	if matrix[i][j]=="S" and matrix[i-1][j]=="S" and matrix[i-2][j]=="S" and matrix[i+1][j]=="S" and matrix[i+2][j]=="S" and matrix[i][j+1]=="S" and matrix[i][j+2]=="S" and matrix[i][j-1]=="S" and matrix[i][j-2]=="S" :
+			print(9,5)
+		
+		elif matrix[i][j]=="S" and matrix[i-1][j]=="S" and matrix[i+1][j]=="S" and matrix[i][j-1]=="S" and matrix[i][j+1]=="S" :
+			print(5,1)
+		
+		else:
+			 print(1)
+
